e10884c41bc62352c7b3fe02f1c8f57d7e1a3f94
Mayee <mayehunt95@gmail.com>
2020-10-06 10:16:19 +0000

    Added code to demonstrate the use of comparison operators.

---
+++ basics/decisions/simple_decision/modulo_operator.py
@@ -0,0 +1,7 @@
+print("Please enter a whole number.")
+num = int(input())
+
+if(num % 2 == 0):
+  print("The number is an even number.")
+elif(num % 2 == 1):
+  print("The number is an odd number.")
