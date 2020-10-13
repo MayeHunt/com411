5d54d4cf5d1ad236052d1fc4ee3ffdc04121c2f9
Mayee <mayehunt95@gmail.com>
2020-10-13 09:48:37 +0000

    Added code to demonstrate the use of a user-defined function with a nested decision.

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,9 @@
+def identify():
+  print("Enter a word representing what you see:")
+  see = input() 
+  if(see == 'a large boulder'):
+    print("It's time to run!")
+  else:
+    print("We will be fine.")
+
+identify() 
