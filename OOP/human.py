f073eb39cdbc087921709e5ebd2c074356c059c4
Mayee <mayehunt95@gmail.com>
2020-12-08 15:11:50 +0000

    Updated classes human and robot with methods to display objects.

--- OOP/human.py
+++ OOP/human.py
@@ -9,6 +9,12 @@ class Human:
     self.age = age
     self.energy = Human.MAX_ENERGY
 
+  #magic methods
+  def __repr__(self):
+    return f'human(name={self.name}, age={self.age}, energy={self.energy})'
+  def __str__(self):
+    return f'Human: {self.name} is {self.age} years old and their energy is {self.energy}.'
+
   #instance methods
   def display(self):
     print(f"I am {self.name}")
@@ -17,3 +23,5 @@ class Human:
 if (__name__ == "__main__"):
   human = Human()
   human.display() 
+  print(repr(human))
+  print(str(human))
--- OOP/robot.py
+++ OOP/robot.py
@@ -9,6 +9,12 @@ class Robot:
     self.age = age
     self.energy = 0
 
+  #magic methods
+  def __repr__(self):
+    return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
+  def __str__(self):
+    return f'Robot: {self.name} is {self.age} years old and its energy is {self.energy}.'
+
   #instance methods
   def display(self):
     print(f"I am {self.name}")
@@ -17,3 +23,5 @@ class Robot:
 if (__name__ == "__main__"):
   robot = Robot()
   robot.display() 
+  print(repr(robot))
+  print(str(robot))
